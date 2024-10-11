72020739a67963e532ad1f8b2e22ee57a7f3412d
vishu pratap <vishurizz01@gmail.com>
2024-10-11 23:53:01 +0530

    siginup added

---
+++ src/Pages/SignupPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Signup from '../components/Signup'
+
+function SignupPage() {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <Signup/>
+    </div>
+  )
+}
+
+export default SignupPage
