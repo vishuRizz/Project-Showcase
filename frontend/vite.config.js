d8d3baee9b21ec7e63fb5990377f94ac11fe8046
vishu pratap <vishurizz01@gmail.com>
2024-10-20 23:17:32 +0530

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        // Manually split vendor modules into separate chunks
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    },
+    // Increase the chunk size warning limit (in KB) if needed
+    chunkSizeWarningLimit: 1000, // Set to 1MB (1000KB)
+  }
+});
